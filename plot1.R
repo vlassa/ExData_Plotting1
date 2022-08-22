8c58b1fb58717f45e5929cc5a696072fa611a112
vlassa <75219350+vlassa@users.noreply.github.com>
2022-08-22 15:59:26 +0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,9 @@
-## Getting full dataset
+# Getting full dataset
 md <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 md$Date <- as.Date(md$Date, format="%d/%m/%Y")
 
-## Subsetting the data
+# Subsetting the data
 md0 <- subset(md, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(md)
 
@@ -11,10 +11,10 @@ rm(md)
 md2 <- paste(as.Date(md0$Date), md0$Time)
 md0$Datetime <- as.POSIXct(md2)
 
-## Plot 1
+# Plot 1
 hist(md0$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
-## Saving to file
+# Saving to file
 dev.copy(png, file="./plot1.png", height=480, width=480)
 dev.off()
